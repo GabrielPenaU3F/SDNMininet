96732da840edc45bf2ee7923ff0c1b85404dc8bb
Gabriel <gpena@untref.edu.ar>
2026-07-21 21:00:01 -0300

    Lil fixes to scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ echo " SDN Mininet Installer"
 echo "========================================="
 echo
 
-echo "[0/5] Checking operating system..."
+echo "[0/6] Checking operating system..."
 
 if [[ ! -f /etc/os-release ]]; then
     echo "Unable to identify the operating system."
@@ -28,36 +28,39 @@ echo "This project was developed and tested on Ubuntu 25.04 LTS."
 echo "Other Ubuntu versions may work but are not officially supported."
 echo
 
-echo "[0/5] Updating..."
+echo "[0/6] Updating..."
 sudo apt update
 echo
 
 echo "Beginning installation..."
 echo
 
-echo "[1/5] Installing Mininet..."
+echo "[1/6] Installing Mininet..."
 sudo apt install -y git curl mininet
 echo "Done"
 echo
 
-echo "[2/5] Installing uv..."
+echo "[2/6] Installing uv..."
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source ~/.local/bin/env
 echo "Done"
 echo
 
-echo "[3/5] Installing Python 3.8..."
+echo "[3/6] Installing Python 3.8..."
 uv python install 3.8
 echo "Done"
 echo
 
-echo "[4/5] Setting up environment..."
+echo "[4/6] Setting up environment..."
 uv venv --python 3.8
 source .venv/bin/activate
 echo "Done"
 echo
 
-echo "[5/5] Resolving dependencies..."
+echo "[5/6] Installing Ryu..."
+uv pip install git+https://github.com/faucetsdn/ryu.git
+
+echo "[6/6] Resolving dependencies..."
 uv pip install -e .
 echo "Done"
 echo
